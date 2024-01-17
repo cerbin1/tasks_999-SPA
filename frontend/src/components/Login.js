cbd1e8ea0c8a3e613b88da21a30e6b297851c98e
cerbin1 <gekarez@gmail.com>
2024-01-17 19:22:39 +0100

    Add component Login

---
+++ frontend/src/components/Login.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+function Login(props) {
+
+  function login(event) {
+    event.preventDefault();
+    alert('login')
+  }
+
+  return <div>
+    <img src={logo} className="App-logo" alt="logo" />
+    <form onSubmit={login}>
+      <input name="username" placeholder="Username" />
+      <input name="password" placeholder="Password" />
+      <button type="submit" >Login</button>
+    </form>
+  </div>
+}
+
+export default Login;
