45eda286aff9369d224217bbe10ffd43da7a2826
cerbin1 <gekarez@gmail.com>
2024-01-17 19:30:11 +0100

    Add skeleton of component HomePage

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import Login from './components/Login'
 import Register from './components/Register'
+import MainPage from './components/MainPage'
 
 function App() {
   return (
     <div className="App">
       {/* <Login/> */}
-      <Register/>
+      {/* <Register/> */}
+      <MainPage/>
     </div>
   );
 }
---
+++ frontend/src/components/MainPage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from '../logo.svg';
+
+function MainPage(props) {
+
+  return <div>
+    <img src={logo} className="App-logo" alt="logo" />
+    <h1>Main Page</h1>
+  </div>
+}
+
+export default MainPage;
