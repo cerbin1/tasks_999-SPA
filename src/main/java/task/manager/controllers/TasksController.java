61fdf469563a2f30e8daf5f7cfa8b4519c191974
cerbin1 <gekarez@gmail.com>
2024-01-19 14:48:35 +0100

    Implement endpoints to create, update and delete task

--- src/main/java/task/manager/controllers/TasksController.java
+++ src/main/java/task/manager/controllers/TasksController.java
@@ -2,17 +2,13 @@ package task.manager.controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import task.manager.entity.Task;
 import task.manager.entity.TasksRepository;
 
 import java.util.Optional;
 
-import static org.springframework.http.HttpStatus.NOT_FOUND;
-import static org.springframework.http.HttpStatus.OK;
+import static org.springframework.http.HttpStatus.*;
 
 @RestController
 @RequestMapping("/tasks")
@@ -31,13 +27,42 @@ public class TasksController {
         return new ResponseEntity<>(allTasks, OK);
     }
 
-
     @GetMapping("/{id}")
     public ResponseEntity<?> getTaskBy(@PathVariable Long id) {
         Optional<Task> task = tasksRepository.findById(id);
-        if (task.isEmpty()) {
-            return new ResponseEntity<>(NOT_FOUND);
+        if (task.isPresent()) {
+            return new ResponseEntity<>(task.get(), OK);
         }
-        return new ResponseEntity<>(task.get(), OK);
+        return new ResponseEntity<>(NOT_FOUND);
     }
+
+    @PostMapping
+    public ResponseEntity<?> create(@RequestBody Task task) {
+        Task taskCreated = tasksRepository.save(task);
+        return new ResponseEntity<>(taskCreated, CREATED);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<?> updateTaskById(@PathVariable Long id, @RequestBody Task task) {
+        if (tasksRepository.existsById(id)) {
+            Task taskUpdated = tasksRepository.save(task);
+            return new ResponseEntity<>(taskUpdated, OK);
+        }
+        return new ResponseEntity<>(NOT_FOUND);
+    }
+
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<?> deleteBy(@PathVariable Long id) {
+        if (tasksRepository.existsById(id)) {
+            Optional<Task> taskToDelete = tasksRepository.findById(id);
+            if (taskToDelete.isPresent()) {
+                tasksRepository.delete(taskToDelete.get());
+                return new ResponseEntity<>(OK);
+            }
+            return new ResponseEntity<>(BAD_REQUEST);
+        }
+        return new ResponseEntity<>(NOT_FOUND);
+    }
+
 }
--- src/test/java/task/manager/controllers/TasksControllerTest.java
+++ src/test/java/task/manager/controllers/TasksControllerTest.java
@@ -2,6 +2,7 @@ package task.manager.controllers;
 
 import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentMatchers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,9 +19,11 @@ import java.util.Optional;
 
 import static org.hamcrest.Matchers.is;
 import static org.mockito.Mockito.when;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static task.manager.utils.ObjectMapperInstance.getObjectMapper;
 
 @SpringBootTest
 @AutoConfigureMockMvc(addFilters = false)
@@ -79,4 +82,96 @@ public class TasksControllerTest {
                 .andExpect(jsonPath("$.priority", Matchers.anEmptyMap()));
     }
 
+    @Test
+    public void shouldReturn400WhenTaskToUpdateWithGivenIdNotFound() throws Exception {
+        // when & then
+        mvc.perform(post("/tasks"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldCreateTask() throws Exception {
+        // given
+        Task task = new Task(1L, "Mike", LocalDateTime.of(2024, 1, 1, 15, 15), new User(), new Priority());
+
+        // when & then
+        mvc.perform(post("/tasks")
+                        .content(getObjectMapper().writeValueAsString(task))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isCreated());
+    }
+
+    @Test
+    public void shouldReturn400WhenThereIsNoTaskObjectInRequestWhenCreatingTask() throws Exception {
+        // given
+        Task task = new Task(1L, "Mike", LocalDateTime.of(2024, 1, 1, 15, 15), new User(), new Priority());
+        when(tasksRepository.existsById(123L))
+                .thenReturn(false);
+
+        // when & then
+        mvc.perform(put("/tasks/123")
+                        .content(getObjectMapper().writeValueAsString(task))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isNotFound());
+    }
+
+    @Test
+    public void shouldUpdateTask() throws Exception {
+        // given
+        Task task = new Task(1L, "Mike", LocalDateTime.of(2024, 1, 1, 15, 15), new User(), new Priority());
+        Task taskToUpdate = new Task(1L, "John", LocalDateTime.of(2024, 1, 10, 10, 10), new User(), new Priority());
+        when(tasksRepository.existsById(1L))
+                .thenReturn(true);
+        when(tasksRepository.save(ArgumentMatchers.any()))
+                .thenReturn(taskToUpdate);
+
+        // when & then
+        mvc.perform(put("/tasks/1")
+                        .content(getObjectMapper().writeValueAsString(task))
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id", is(1)))
+                .andExpect(jsonPath("$.name", is("John")))
+                .andExpect(jsonPath("$.deadline", is("2024-01-10T10:10:00")))
+                .andExpect(jsonPath("$.assignee", Matchers.anEmptyMap()))
+                .andExpect(jsonPath("$.priority", Matchers.anEmptyMap()));
+    }
+
+    @Test
+    public void shouldDeleteTask() throws Exception {
+        // given
+        Task task = new Task(1L, "Mike", LocalDateTime.of(2024, 1, 1, 15, 15), new User(), new Priority());
+        when(tasksRepository.existsById(1L))
+                .thenReturn(true);
+        when(tasksRepository.findById(1L))
+                .thenReturn(Optional.of(task));
+
+        // when & then
+        mvc.perform(delete("/tasks/1"))
+                .andExpect(status().isOk());
+    }
+
+    @Test
+    public void shouldReturn400WhenTryingDeleteNotExistingTask() throws Exception {
+        // given
+        when(tasksRepository.existsById(1L))
+                .thenReturn(true);
+        when(tasksRepository.findById(1L))
+                .thenReturn(Optional.empty());
+
+        // when & then
+        mvc.perform(delete("/tasks/1"))
+                .andExpect(status().isBadRequest());
+    }
+
+    @Test
+    public void shouldReturn404WhenTaskByIdToDeleteNotExists() throws Exception {
+        // given
+        when(tasksRepository.existsById(1L))
+                .thenReturn(false);
+
+        // when & then
+        mvc.perform(delete("/tasks/1"))
+                .andExpect(status().isNotFound());
+    }
 }
