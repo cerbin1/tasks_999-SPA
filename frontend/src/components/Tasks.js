f7cc5d1ab6c26a26a4d0026bd7e6a73335b281f9
cerbin1 <gekarez@gmail.com>
2024-01-29 12:39:05 +0100

    Use backend endpoint to list all tasks instead of hardcoded values

--- frontend/src/components/Tasks.js
+++ frontend/src/components/Tasks.js
@@ -1,21 +1,37 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function Tasks(props) {
-  const list = [
-    { id: 1, name: 'task 1', deadline: new Date(), assigned: 'Mike', priority: 1 },
-    { id: 2, name: 'task 2', deadline: new Date(), assigned: 'Mike', priority: 2 },
-    { id: 3, name: 'task 3', deadline: new Date(), assigned: 'Mike', priority: 3 },
-    { id: 4, name: 'task 4', deadline: new Date(), assigned: 'Mike', priority: 1 },
-    { id: 5, name: 'task 5', deadline: new Date(), assigned: 'Mike', priority: 2 },
-  ];
+  const [data, setData] = useState([])
 
-  const listOfElements = list.map((task) =>
+
+
+  const apiUrl = 'http://localhost:8080/tasks';
+
+  useEffect(() => {
+
+    fetch(apiUrl)
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+
+        return response.json();
+      })
+      .then(data => {
+        setData(data)
+      })
+      .catch(error => {
+        alert(error)
+      });
+  }, []);
+
+  const listOfElements = data.map((task) =>
     <tr key={task.id}>
       <td>{task.id}</td>
       <td>{task.name}</td>
       <td>{task.deadline.toString()}</td>
-      <td>{task.assigned}</td>
-      <td>{task.priority}</td>
+      <td>{task.assignee.name}</td>
+      <td>{task.priority.value}</td>
     </tr>
   );
 
