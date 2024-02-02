f0e98e414051f12a05a354991d8c4598e9e829c4
cerbin1 <gekarez@gmail.com>
2024-02-02 14:09:50 +0100

    Validate Login component

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -3,7 +3,8 @@ import logo from '../logo.svg';
 import { useNavigate } from 'react-router-dom';
 
 function Login(props) {
-  const [user, setUser] = useState({})
+  const [user, setUser] = useState({ username: '', password: '' });
+  const [errors, setErrors] = useState();
 
   const apiUrl = 'http://localhost:8080/auth/';
 
@@ -15,9 +16,25 @@ function Login(props) {
       [event.target.id]: event.target.value
     })
   }
+  function validateValues() {
+    let errors = {};
+    if (user.username.length == 0) {
+      errors.username = true;
+    }
+    if (user.password.length == 0) {
+      errors.password = true;
+    }
+    return errors;
+  };
 
   function login(event) {
     event.preventDefault();
+
+    const errorValues = validateValues();
+    setErrors(errorValues)
+    if (Object.keys(errorValues).length !== 0) {
+      return;
+    }
 
     fetch(apiUrl + 'login', {
       method: 'POST',
@@ -28,9 +45,8 @@ function Login(props) {
     })
       .then(response => {
         if (!response.ok) {
-          throw new Error('Network response was not ok');
+          throw new Error('Nie udało się zalogować.')
         }
-        return response.json();
       })
       .then(data => {
         localStorage.setItem("token", data.token);
@@ -47,10 +63,20 @@ function Login(props) {
       <div className="form-group">
         <label htmlFor="username">Username</label>
         <input className="form-control" id="username" value={user.username} placeholder="Enter username" onChange={handleChange} />
+        {errors && errors.username &&
+          <div className="alert alert-danger" role="alert">
+            You must enter username before submitting.
+          </div>
+        }
       </div>
       <div className="form-group">
         <label htmlFor="password">Password</label>
         <input type="password" className="form-control" id="password" value={user.password} placeholder="Password" onChange={handleChange} />
+        {errors && errors.password &&
+          <div className="alert alert-danger" role="alert">
+            You must enter username password submitting.
+          </div>
+        }
       </div>
       <button type="submit" className="btn btn-primary">Login</button>
     </form>
