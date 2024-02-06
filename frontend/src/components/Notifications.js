3edb5cef3bd46cba435065256977a96d3e67aa46
cerbin1 <gekarez@gmail.com>
2024-02-06 12:16:59 +0100

    Implement button for marking notification as read

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -50,6 +50,24 @@ function Notifications(props) {
       });
   }
 
+  function handleMarkNotificationAsRead(id) {
+    fetch(apiUrl + '/' + id + '/read', {
+      method: 'PUT',
+      headers: {
+        "Authorization": `Bearer ` + localStorage.getItem('token'),
+      }
+    })
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        loadNotifications();
+      })
+      .catch(error => {
+        alert(error)
+      });
+  }
+
   const listOfNotifications = data.map((notification) =>
     <tr key={notification.id}>
       <th scope="row">{notification.id}</th>
@@ -58,6 +76,9 @@ function Notifications(props) {
       <td>{notification.read ? "Yes" : "No"}</td>
       <td>{notification.readDate.toString()}</td>
       <td>{notification.user.name} {notification.user.surname}</td>
+      <td>
+        <button type="button" className="btn btn-primary" onClick={() => handleMarkNotificationAsRead(notification.id)}>Read</button>
+      </td>
       <td>
         <button type="button" className="btn btn-danger" onClick={() => handleRemove(notification.id)}>Remove</button>
       </td>
@@ -74,6 +95,7 @@ function Notifications(props) {
           <th scope="col">Is read</th>
           <th scope="col">Read date</th>
           <th scope="col">User assigned</th>
+          <th scope="col">Mark as read</th>
           <th scope="col">Remove</th>
         </tr>
       </thead>
