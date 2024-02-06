f228b4dc9f49fe2894df86cfe445f128575a2075
cerbin1 <gekarez@gmail.com>
2024-02-06 12:44:58 +0100

    Add bold text to info about not read notification

--- frontend/src/components/Notifications.js
+++ frontend/src/components/Notifications.js
@@ -73,7 +73,7 @@ function Notifications(props) {
       <th scope="row">{notification.id}</th>
       <td>{notification.name}</td>
       <td>{notification.createDate.toString()}</td>
-      <td>{notification.read ? "Yes" : "No"}</td>
+      <td>{notification.read ? "Yes" : <b>No</b>}</td>
       <td>{notification.readDate && notification.readDate.toString()}</td>
       <td>{notification.user.name} {notification.user.surname}</td>
       <td>
