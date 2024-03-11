d237b0cd78bdd19529b294b07e42eae2476fa783
cerbin1 <gekarez@gmail.com>
2024-03-11 15:35:45 +0100

    Make navbar expand automatically from medium size screen instead of large

--- frontend/src/components/LoggedInUserLayout.js
+++ frontend/src/components/LoggedInUserLayout.js
@@ -39,7 +39,7 @@ const LoggedInUserLayout = () => {
 
   return (
     <>
-      <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <nav className="navbar navbar-expand-md navbar-light bg-light">
         <a className="navbar-brand" href="#">Navbar</a>
         <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
